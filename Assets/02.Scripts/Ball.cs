4479009d216d09c5fc09cad9809954daeb00e155
Jin Park <jiny199109@gmail.com>
2019-03-07 22:55:41 +0900

    Ball UI

---
+++ Assets/02.Scripts/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+	private bool _isSelected = false;
+	private const float Radius = 3.0f;
+	private const float ShortRadius = 0.866025f;
+	private const float Slope = 1.73205f;
+
+	private void OnMouseDown()
+	{
+		_isSelected = true;
+	}
+
+	private void OnMouseDrag()
+	{
+		if (!_isSelected) return;
+
+		var lastPosition = transform.localPosition;
+
+		// Let it follow cursor.
+		var positionOfScreen = Camera.main.WorldToScreenPoint(transform.position);
+		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, positionOfScreen.z));
+
+		// If it's out of hexagon
+		var x = transform.localPosition.x;
+		var y = transform.localPosition.y;
+		var distance = Mathf.Sqrt(x * x + y * y);
+		if (y > ShortRadius || y < -ShortRadius ||
+		    y > -Slope * x + Slope ||
+		    y < +Slope * x - Slope ||
+		    y < -Slope * x - Slope ||
+		    y > +Slope * x + Slope)
+		{
+			// Back to last position
+			transform.localPosition = lastPosition;
+		}
+	}
+
+	private void OnMouseUp()
+	{
+		_isSelected = false;
+	}
+}
---
+++ Assets/02.Scripts/EmotionPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmotionPanel : MonoBehaviour
+{
+	public bool MouseIsOn = false;
+
+	private void OnMouseEnter()
+	{
+		MouseIsOn = true;
+	}
+
+	private void OnMouseExit()
+	{
+		MouseIsOn = false;
+	}
+
+	private void OnMouseOver()
+	{
+		MouseIsOn = true;
+	}
+}
