686c6c449579aee3ea0294e77e3478bff2fd9bf3
Jin Park <jiny199109@gmail.com>
2019-03-08 13:49:53 +0900

    DLL injection

---
+++ Assets/02.Scripts/InputListner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class InputListner : MonoBehaviour
+{
+	private InputField _input;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		_input = gameObject.GetComponent<InputField>();
+		_input.onEndEdit.AddListener(InputFieldListener);
+	}
+
+	private void InputFieldListener(string input)
+	{
+		if (Add1(1) == 2)
+		{
+			_input.text = "DLL INJECTED!";
+		}
+	}
+
+	[DllImport("testdll")]
+	public static extern int Add1(int input);
+}
