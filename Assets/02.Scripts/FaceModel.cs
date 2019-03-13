68352028833dde2bf28aa85e455a490d13b7d0eb
Jin Park <jiny199109@gmail.com>
2019-03-13 22:56:19 +0900

    TestCube vertex manipulating

---
+++ Assets/02.Scripts/FaceModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceModel : MonoBehaviour
+{
+	private Mesh _faceMesh;
+
+	void Start()
+	{
+		_faceMesh = transform.Find("TestCube").GetComponent<MeshFilter>().mesh;
+	}
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+	    var vertices = _faceMesh.vertices;
+	    var normals = _faceMesh.normals;
+	    int j=0;
+	    for (int i = 0; i < vertices.Length; i++)
+	    {
+            			print("time" +  " : "+Time.time);
+            vertices[i] -= normals[i] * Mathf.Sin(Time.time*10)/10;
+	    }
+        
+        _faceMesh.vertices = vertices;
+    }
+}
