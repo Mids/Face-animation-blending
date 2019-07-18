6d83afc248c51b283e80609f8ccb2be565eec254
Jin Park <jiny199109@gmail.com>
2019-07-18 17:04:23 +0900

    Relative path

--- Assets/02.Scripts/Constants.cs
+++ Assets/02.Scripts/Constants.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public static class Constants
 {
-	public const string WaveFilePath = "file:///C:/Users/jiny1/Documents/Unity3D/Face anim 32bit/";
-	public const string WaveFileName = "byte.wav";
+	// TODO: dataPath is different on each platform. If you are in OSX, change it to "/../../"
+	public static readonly string WaveFilePath = "file:///" + Application.dataPath + "/../";
+	public static readonly string WaveFileName = "byte.wav";
 }
